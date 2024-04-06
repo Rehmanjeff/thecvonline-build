cca62830200e478103151619ca2e05abdf797a3d
Quwat <Quwat@TimeAhead.co>
2024-04-06 20:29:20 +0500

    revert

--- src/components/LayoutGuest.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="bg-slate-800 text-slate-100">
-    <slot />
-  </div>
-</template>
--- src/plugins/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
-axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
-axios.defaults.headers.common['Content-Type'] = 'multipart/form-data';
-
-export default Vue => Vue.provide('$axios', axios);
